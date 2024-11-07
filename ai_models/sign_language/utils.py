36d6cf200c6fdee2f670d5905f64e159771e5c29
np4s <npanh1104@gmail.com>
2024-11-08 03:05:34 +0700

    add testing

--- ai_models/sign_language/utils.py
+++ ai_models/sign_language/utils.py
@@ -1,11 +1,20 @@
 from spoken_to_signed.bin import _text_to_gloss, _gloss_to_pose, _pose_to_video
 from pose_format import Pose
 from pose_format.pose_visualizer import PoseVisualizer
+import underthesea
 
-LEXICON_DIR = "spoken-to-signed-translation/assets/"
+LEXICON_DIR = "assets/"
+POSE_FOLDER = {
+    "vsl": "vi_lexicon",
+}
 
-
-def to_pose(text, output_dst, src_lang="de", dst_lang="sgg", glosser="simple"):
+def vi_gloss(text):
+    text = underthesea.text_normalize(text)
+    text = underthesea.word_tokenize(text)
+    gloss = [[(word, word) for word in text]]
+    return gloss
+    
+def to_pose(text, output_dst, src_lang="vi", dst_lang="vsl", glosser="simple"):
     '''
     text: input sentence
     output_dst: name of the .pose output file
@@ -14,9 +23,12 @@ def to_pose(text, output_dst, src_lang="de", dst_lang="sgg", glosser="simple"):
     glosser: ['simple', 'spacylemma', 'rules', 'nmt'] glossing technique. current implementation only supports 'simple' - simple lookup from spoken to gloss
     '''
     lexicon = LEXICON_DIR + \
-        "dummy_lexicon"  # directory of the translated lexicon, including indexing csv
+        POSE_FOLDER[dst_lang]  # directory of the translated lexicon, including indexing csv
 
-    sentences = _text_to_gloss(text, src_lang, glosser)
+    if src_lang == "vi":
+        sentences = vi_gloss(text)
+    else:
+        sentences = _text_to_gloss(text, src_lang, glosser)
     pose = _gloss_to_pose(sentences, lexicon, src_lang, dst_lang)
 
     with open(output_dst, "wb") as f:
