a220e93eb03c68ebabf432e72b19a2d026dd5fb9
chitrung-gg <doremob311205@gmail.com>
2024-12-01 16:37:53 +0700

    Adding more changes

--- frontend/src/app/(pages)/meeting/enter-meeting/page.tsx
+++ frontend/src/app/(pages)/meeting/enter-meeting/page.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 import ToolBar from "@/app/component/enter-meeting/ToolBar";
 import VideoArea from "@/app/component/enter-meeting/VideoArea";
+import MessageComponent from "@/app/component/messaging/Message";
 const VideoCallComponent = () => {
   return (
     <div className="flex flex-col h-screen bg-gray-900 text-white">
       {/* Video Area */}
       <VideoArea />
+      {/* <MessageComponent/> */}
       {/* Toolbar */}
       <ToolBar />
     </div>
